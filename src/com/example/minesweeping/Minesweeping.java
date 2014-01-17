f420e55477aaae760036418cc5bd940ab677c322
xinkuokuo <xinkuokuo@126.com>
2014-01-17 17:25:58 +0800

    An android application

--- src/com/example/minesweeping/Minesweeping.java
+++ src/com/example/minesweeping/Minesweeping.java
@@ -1,17 +1,18 @@
 package com.example.minesweeping;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.ViewGroup.LayoutParams;
+import android.util.DisplayMetrics;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
 
 public class Minesweeping extends Activity {
 	private TableLayout mineField;
-	private block blocks[][];
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -19,18 +20,23 @@ public class Minesweeping extends Activity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.main);
+		DisplayMetrics dm= new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+		int width = dm.widthPixels;
 		mineField=(TableLayout)findViewById(R.id.MineField);
-	//	for (int row = 0; row < 5; row++)
-	//	{
-			TableRow tableRow = new TableRow(this);
-			tableRow.setLayoutParams(new LayoutParams(1,1));
-		//	for(int column = 0; column < 5; column++)
-	//		{
-//				blocks[row][column].setLayoutParams(new LayoutParams(12,12));
-//				blocks[row][column].setPadding(1, 1, 1, 1);
-//				tableRow.addView(blocks[row][column]);
-//			}
-			mineField.addView(tableRow,new TableLayout.LayoutParams(1,1));
-	//	}
+	    for(int row = 0; row < 4; row++)
+	    {
+	    	TableRow tableRow=new TableRow(this);
+	    	tableRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT,TableRow.LayoutParams.FILL_PARENT));
+	    	for(int col = 0;col < 10;col++)
+	    	{
+	    		Button tv = new Button(this);
+	    		tv.setHeight(width/10);
+	    		tv.setBackgroundColor(Color.parseColor("#F5F5DC"));
+	    		tableRow.addView(tv);
+	    	}
+	    	tableRow.setPadding(3, 3, 3, 3);
+	    	mineField.addView(tableRow,new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT,TableLayout.LayoutParams.FILL_PARENT));
+	    }
 	}
 	}
---
+++ src/com/example/minesweeping/block.java
@@ -0,0 +1,9 @@
+package com.example.minesweeping;
+import android.content.Context;
+import android.widget.Button;
+ 
+public class block extends Button { 
+    public block(Context context) {
+        super(context);
+    }
+}
