36c26893721f8c60495a7db1089c06b9c841aa2d
xinkuokuo <xinkuokuo@126.com>
2014-01-19 00:37:00 +0800

    An android application

--- src/com/example/minesweeping/Minesweeping.java
+++ src/com/example/minesweeping/Minesweeping.java
@@ -1,7 +1,10 @@
+/*********************************
+ *  THIS IS AN ANDROID APPLICATION
+ *  A WORLD MINESWEEPING GAME
+ ********************************/
 package com.example.minesweeping;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Window;
@@ -20,23 +23,28 @@ public class Minesweeping extends Activity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.main);
-		DisplayMetrics dm= new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics(dm);
-		int width = dm.widthPixels;
 		mineField=(TableLayout)findViewById(R.id.MineField);
-	    for(int row = 0; row < 4; row++)
-	    {
-	    	TableRow tableRow=new TableRow(this);
-	    	tableRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT,TableRow.LayoutParams.FILL_PARENT));
-	    	for(int col = 0;col < 10;col++)
-	    	{
-	    		Button tv = new Button(this);
-	    		tv.setHeight(width/10);
-	    		tv.setBackgroundColor(Color.parseColor("#F5F5DC"));
-	    		tableRow.addView(tv);
-	    	}
-	    	tableRow.setPadding(3, 3, 3, 3);
-	    	mineField.addView(tableRow,new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT,TableLayout.LayoutParams.FILL_PARENT));
-	    }
+	    creatMine(10,mineField);
+	}
+	
+	public DisplayMetrics getDisplayMertics(){
+		DisplayMetrics dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+		return dm;
+	}
+	
+	private void creatMine(int rownumber, TableLayout mineField){
+		for(int row = 0;row < rownumber;row++){
+			TableRow tableRow = new TableRow(this);
+			tableRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT,TableRow.LayoutParams.WRAP_CONTENT));
+			for(int col = 0;col < rownumber;col++){
+				Button mine= new Button(this);
+				mine.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT,getDisplayMertics().widthPixels/rownumber));
+				mine.setBackgroundResource(R.drawable.minebuttongreen);
+				tableRow.addView(mine);
+			}
+			tableRow.setPadding(0, 0, 0, 0);
+			mineField.addView(tableRow,new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT,TableLayout.LayoutParams.FILL_PARENT));
+		}
 	}
 	}
--- src/com/example/minesweeping/block.java
+++
@@ -1,9 +0,0 @@
-package com.example.minesweeping;
-import android.content.Context;
-import android.widget.Button;
- 
-public class block extends Button { 
-    public block(Context context) {
-        super(context);
-    }
-}
